7b7bf3cf9b381f2d42d46783f8c584e57d68ecaa
Aadarsh Malviya <malviyaaadarsh45@gmail.com>
2025-11-23 17:51:06 +0530

    initial commit

---
+++ react-router/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Navbar from './components/Navbar.jsx'
+import Products from './pages/Product.jsx'
+const App = () => {
+  return (
+    <div>
+     <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/products' element={<Products />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ react-router/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+       <a href='/'><h1>Home Page</h1></a> 
+     
+    </div>
+  )
+}
+
+export default Home
